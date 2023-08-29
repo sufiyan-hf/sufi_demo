b156a7a0c00d135daef4750df14ffad51101de3d
MaheshSwami2002 <mahesh.swami@heinfricke.team>
2023-08-29 15:01:27 +0530

    develpoment added

--- main.java
+++ main.java
@@ -1,7 +1,9 @@
 public class main{
 
 public static void main(String[] args) {
-    System.out.println("my name is sufiyan ");
+    System.out.println("my name is Mahesh");
+    System.out.println("i live in pune");
+    System.out.println("i live in Nanded");
     System.out.println("i live in pune");
 }
 }

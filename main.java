50f3174a97dd8281f518ba7ffff0834a9d7c2eef
sufiyan-hf <sufiyan.khan@heinfricke.team>
2023-08-29 03:45:05 -0700

    merged main again

--- main.java
+++ main.java
@@ -3,12 +3,9 @@ public class main{
 public static void main(String[] args) {
     System.out.println("my name is habib ");
     System.out.println("i live in lonavala");
-<<<<<<< HEAD
     System.out.println("my name is Mahesh");
     System.out.println("i live in pune");
     System.out.println("i live in Nanded");
     System.out.println("i live in pune");
-=======
->>>>>>> 8f581e41fa4b32a8a149955335981dc1ad380288
 }
 }

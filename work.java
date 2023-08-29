d1085feb68c59794eb3add80715fb268c56bb6d9
sufiyan-hf <sufiyan.khan@heinfricke.team>
2023-08-29 04:39:13 -0700

    habib modified work

--- work.java
+++ work.java
@@ -1,7 +1,8 @@
 public class work {
     public static void main(String[] args) {
-        System.out.println("hi, sufiyan");
+        System.out.println("hi, i am habib");
         System.out.println("i love cricket");
+        System.out.println("i love coding");
     }
     
 }

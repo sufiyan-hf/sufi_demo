376e67b30d0b525b8907edd50c54b12ce1230e8b
sufiyan-hf <sufiyan.khan@heinfricke.team>
2023-08-29 02:50:09 -0700

    main modified

--- main.java
+++ main.java
@@ -1,6 +1,8 @@
 public class main{
 
 public static void main(String[] args) {
+    System.out.println("my name is habib ");
+    System.out.println("i live in lonavala");
     System.out.println("my name is Mahesh");
     System.out.println("i live in pune");
     System.out.println("i live in Nanded");

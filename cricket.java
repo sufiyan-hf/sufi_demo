de4b95530acab00be1e598b47b7b38b10aad989a
MaheshSwami2002 <mahesh.swami@heinfricke.team>
2023-08-30 11:02:04 +0530

    changes in cricket

--- cricket.java
+++ cricket.java
@@ -1,7 +1,8 @@
 public class cricket {
     public static void main(String[] args) {
-        System.out.println("i am sufiyan");
-        System.out.println("i love cricket");
+        System.out.println("i am mahesh");
+        System.out.println("i love football");
+ System.out.println("i love cri");
     }
 
 }
